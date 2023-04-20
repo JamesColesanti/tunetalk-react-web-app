a9c02d84a44320cb480f065447399b848af2e026
liamsmeraglia <liamsmeraglia1225@gmail.com>
2023-04-20 16:25:15 -0400

    added some dummy routes

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function HomePage () {
+    return (
+        <div className="HomePage">
+            <h1>Welcome to the HomePage</h1>
+        </div>
+    );
+}
+export default HomePage;
---
+++ src/pages/SearchResultsPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function SearchResultsPage () {
+    return (
+        <div className="SearchResultsPage">
+            <h1>Welcome to the SearchResultsPage</h1>
+        </div>
+    );
+}
+export default SearchResultsPage;
